98acec205c0260577bdc4dd5dd72b6750b219a8b
virgilio-ferreira <virgilio.ferreiraneto@gmail.com>
2020-08-27 17:41:38 -0300

    Inicio JS

---
+++ bloco4/aula1/scripts.js
@@ -0,0 +1,16 @@
+// let idade = 5;
+// console.log(idade);
+// let nome = "Virgilio"
+// console.log(nome);
+// let altura = 180;
+// console.log(altura);
+// let nome, idade, altura;
+
+
+let x = 3.1416;
+x = 3.141;
+console.log(x); // retorna 3.14
+
+// const pi = 3.1416;
+// pi = 3.14; // retorna erro
+// console.log(pi); 
