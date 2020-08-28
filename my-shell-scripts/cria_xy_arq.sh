765ecf71febf18d087116c08a9ae2bb538be46b3
virgilio-ferreira <virgilio.ferreiraneto@gmail.com>
2020-08-27 22:18:32 -0300

    dando commit, nos arq deletados

--- my-shell-scripts/cria_xy_arq.sh
+++ my-shell-scripts/cria_xy_arq.sh
@@ -1,3 +1,6 @@
+#Execute arquivo.sh x y
+#Cria x arquivos.js na pasta y
+
 mkdir $2
 cat cont.txt > cont.txt
 cont=1
@@ -8,6 +11,8 @@ do
 done
 for i in `cat cont.txt`
 do
-    echo "#!/bin/bash" > "./$2/exercise${i}.sh"
-    chmod +x "./$2/exercise${i}.sh"
+    echo "//exercicio${i}" > "./$2/exercise${i}.js"
 done
+
+rm cont.txt
+# creditos vladimir chagas
