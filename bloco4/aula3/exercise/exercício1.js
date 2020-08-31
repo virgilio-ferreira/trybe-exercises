e8914cc56cf0555b051bfe2821b4650020f02055
virgilio-ferreira <virgilio.ferreiraneto@gmail.com>
2020-08-31 15:50:43 -0300

    primeiros exercíscios

--- bloco4/aula3/exercise/exercício1.js
+++ bloco4/aula3/exercise/exercício1.js
@@ -1,7 +1,12 @@
 //exercicio1
 
-cont n=3
-
-
-
-
+const n = 3;
+let quadrado = []
+let linha = []
+for (let i=0; i<n; i += 1){
+    linha[i]= '*';
+}
+for (let i=0; i<n; i += 1){
+    quadrado[i] = [linha];
+}
+console.log(quadrado);
--- bloco4/aula3/exercise/exercício2.js
+++ bloco4/aula3/exercise/exercício2.js
@@ -1,1 +1,18 @@
 //exercicio2
+
+const n = 5;
+
+let quadrado = []
+let linha = []
+
+// for (let i=0; i<n; i += 1){
+//     linha[i]= '*';
+//     console.log(linha);
+// }
+
+// Porque esse debaixo dá erro é um mistério:
+for (let i=0; i<n; i += 1){
+   linha[i]= '*';
+   quadrado[i] = [linha];
+}
+console.log(quadrado);
