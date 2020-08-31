ccf4b8a8b98377392f2c787bd247fcb5f0e5ccf2
virgilio-ferreira <virgilio.ferreiraneto@gmail.com>
2020-08-31 18:30:32 -0300

    exercisios 1 e2

--- bloco4/aula1/exercicios-JS/exercicios2.js
+++ bloco4/aula1/exercicios-JS/exercicios2.js
@@ -1,1 +1,23 @@
 //exercicio2
+
+n1 = 2;
+n2 = 300;
+n3 = 45;
+
+if (n1>n2){
+    if (n1>n3){
+        console.log('O maior numero é o `$n1')
+    }
+    else{
+        console.log('o maior numero é o n3')
+    }
+}
+else {
+    if (n2>n3){
+        console.log('O maior numero é o `$n2')
+    }
+    else{
+        console.log('o maior numero é o n3')
+    }
+}
+
