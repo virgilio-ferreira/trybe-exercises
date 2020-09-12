956960ca28a7ef0723c62a2c805c1eb8dd61da7d
virgilio-ferreira <virgilio.ferreiraneto@gmail.com>
2020-09-12 11:33:13 -0300

    exercicio 2 e 3 do code pen aula 5.3

--- bloco5/aula3/exercises/exerciseCodePen3.js
+++ bloco5/aula3/exercises/exerciseCodePen3.js
@@ -1,1 +1,14 @@
 //exercicio3
+const caixaTexto = document.querySelector('.caixa-texto');
+const caixaUm = document.querySelector('.caixa1');
+const caixaDois = document.querySelector('.caixa2');
+
+function trocaTextoA () {
+  caixaUm.innerText = caixaTexto.value;
+}
+function trocaTextoB () {
+  caixaDois.innerText = caixaTexto.value;
+}
+//Crie seu event listener abaixo:
+caixaTexto.addEventListener("input",trocaTextoA);
+caixaTexto.addEventListener("change",trocaTextoB);
