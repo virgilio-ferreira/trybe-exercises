fcb03adef27e6df0e7ca8c184b0ccd4afe4f2dd1
virgilio-ferreira <virgilio.ferreiraneto@gmail.com>
2020-10-05 11:44:54 -0300

    exercicio 4 da segunda parte do teste

--- bloco7/aula3/segundo bloco/exercise4_test.js
+++ bloco7/aula3/segundo bloco/exercise4_test.js
@@ -1,13 +1,48 @@
 // Exercício 4 da segunda parte do bloco
 
+const assert = require('assert');
+
 function secondThirdSmallest(array) {
-    let results = []
-    array.sort(function (x, y) {
-        return x + y;
-    });
-    results = [array[1], array[2]];
-    return results;
+	let results = []
+	array.sort(function (x, y) {
+			return x - y;
+	});
+	results = [array[1], array[2]];
+	return results;
 };
 
 const parameter = [4, 10, 32, 9, 21, 90, 5, 11, 8, 6];
 const result = [5, 6];
+
+assert.deepStrictEqual(secondThirdSmallest(parameter), result);
+
+
+// depois de procurar na documentação o sort consegue ordenar facilmente
+// obortei essa tentaiva abaixo
+// function secondThirdSmallest(numbers) {
+// 	let results = []
+// 	let maior =0;
+// 	for (let i=0; i<numbers.length; i += 1){
+// 		if (numbers [i] > maior){
+// 			maior = numbers [i];
+// 		}
+// 	}
+// 	let menor = [];
+// 	menor=maior;
+// 	for (let i=0; i<numbers.length; i += 1){
+// 		if (numbers [i] < menor){
+// 			menor = numbers [i];
+// 		}
+// 	}
+
+// 	console.log(`O menor valor dessa parada é ${menor}`);
+// 	results = [numbers[1], numbers[2]];
+// 	console.log(results);
+// 	return results;
+// };
+
+
+// const parameter = [4, 10, 32, 9, 21, 90, 5, 11, 8, 6];
+// const result = [5, 6];
+
+// secondThirdSmallest(parameter);
