96b4ce4669dcb90dfd545d385e89b378e933d01b
virgilio-ferreira <virgilio.ferreiraneto@gmail.com>
2020-11-04 20:40:30 -0300

    Bloco 8.4 ex 7 e 8

--- exercises/higher-order-functions-do-javascript-es6_4/exercises8.4.8.js
+++ exercises/higher-order-functions-do-javascript-es6_4/exercises8.4.8.js
@@ -1,1 +1,10 @@
 //exercicio8
+const assert = require('assert')
+
+// Escreva uma função greet que, dado o nome de uma pessoa, retorna uma mensagem de cumprimento:
+
+const greet = (name = 'Stranger',greeting = 'Hi') => `${greeting} ${name}` // importante ter certeza o tipo da variável pra colocar {} ou [] ou ()
+
+assert.equal(greet("John"), "Hi John")
+assert.equal(greet("John", "Good morning"), "Good morning John")
+assert.equal(greet("Isabela", "Oi"), "Oi Isabela")
