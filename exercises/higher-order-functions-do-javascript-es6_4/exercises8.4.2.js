09063230e065ce8cf223d3459acead311bc48b2f
virgilio-ferreira <virgilio.ferreiraneto@gmail.com>
2020-11-04 19:10:50 -0300

    Bloco 8.4 ex 2

--- exercises/higher-order-functions-do-javascript-es6_4/exercises8.4.2.js
+++ exercises/higher-order-functions-do-javascript-es6_4/exercises8.4.2.js
@@ -1,1 +1,10 @@
 //exercicio2
+const assert = require('assert')
+
+const sum = (...arr) => arr.reduce((acc, curr)=> acc+ curr, 0)
+
+assert.equal(sum(), 0)
+assert.equal(sum(1), 1)
+assert.equal(sum(1, 2), 3)
+assert.equal(sum(1, 2, 3), 6)
+assert.equal(sum(1, 2, 3, 4), 10)
