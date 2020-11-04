0ba46c8fe8e01cee3865584ee0340dc457e81949
virgilio-ferreira <virgilio.ferreiraneto@gmail.com>
2020-11-04 19:03:30 -0300

    Bloco 8.4 ex 1

---
+++ exercises/higher-order-functions-do-javascript-es6_4/exercises8.4.1.js
@@ -0,0 +1,15 @@
+//exercicio1
+const assert = require('assert')
+
+const rectangleArea = (width, height) => width * height
+
+const rectangle1 = [1, 2]
+const rectangle2 = [3, 5]
+const rectangle3 = [6, 9]
+const rectangles = [rectangle1, rectangle2, rectangle3]
+
+// Dado o código abaixo, complete-o de forma que seja impressa a área dos 3 retângulos. 
+for(rectangle of rectangles) {
+    assert.equal(rectangleArea(...rectangle), rectangle[0]*rectangle[1]) // altere a chamada da funcao rectangleArea
+}
+// oq é esse "rectangle of rectangles"?
