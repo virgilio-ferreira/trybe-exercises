2abd321358c711a47a3ab6f4a5bc9bdedbd1c51b
virgilio-ferreira <virgilio.ferreiraneto@gmail.com>
2020-11-08 17:44:13 -0300

    exercicio 3

--- exercises/javascript-assincrono-e-promises_1/exercises9.1.3.js
+++ exercises/javascript-assincrono-e-promises_1/exercises9.1.3.js
@@ -1,1 +1,14 @@
 //exercicio3
+const getPlanet = () => {
+  const mars = {
+    name: "Mars",
+    distanceFromSun: {
+      value: 227900000,
+      measurementUnit: "kilometers",
+    },
+  };
+  console.log("Returned planet: ", mars);
+};
+// A função getPlanet abaixo imprime o planeta Marte de forma síncrona. Modifique getPlanet, de forma que Marte seja impresso assincronamente, depois de 4 segundos.
+getPlanet(); // imprime Marte depois de 4 segundos
+setTimeout(() => getPlanet(), 4000); // Obs: o prametro da setTimeOut precisa ser uma callback
