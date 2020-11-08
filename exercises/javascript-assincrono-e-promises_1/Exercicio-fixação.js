1d7a68a94d91ae214d67002a4ef4562c9a281681
virgilio-ferreira <virgilio.ferreiraneto@gmail.com>
2020-11-08 15:17:08 -0300

    exercicio fixação 2

---
+++ exercises/javascript-assincrono-e-promises_1/Exercicio-fixação.js
@@ -0,0 +1,17 @@
+const assert = require('assert');
+
+const userFullName = ({ firstName, lastName }) => `Hello! My name is ${firstName} ${lastName}`;
+const userNationality = ({ firstName, nationality }) => `${firstName} is ${nationality}`;
+
+const getUser = () => {
+  const userToReturn = {
+    firstName: "Ivan",
+    lastName: "Ivanovich",
+    nationality: "Russian"
+  };
+  // return userFullName(userToReturn);
+  return userNationality(userToReturn);
+};
+
+// assert.equal(getUser(), "Hello! My name is Ivan Ivanovich"); // complete a chamada da função de getUser
+assert.equal(getUser(), "Ivan is Russian"); // complete a chamada da função de getUser
